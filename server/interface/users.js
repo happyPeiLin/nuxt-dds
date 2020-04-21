91e40f51113aade9520001cd8de68d44bc5fb597
happyPeiLin <18398322316@163.com>
2020-04-22 01:21:44 +0800

    8-5

--- server/interface/users.js
+++ server/interface/users.js
@@ -6,19 +6,12 @@ import Passport from './utils/passport'
 import Email from '../dbs/config'
 import axios from './utils/axios'
 
-let router = new Router({
-  prefix: '/users'
-})
+let router = new Router({prefix: '/users'})
 
 let Store = new Redis().client
 
 router.post('/signup', async (ctx) => {
-  const {
-    username,
-    password,
-    email,
-    code
-  } = ctx.request.body;
+  const {username, password, email, code} = ctx.request.body;
 
   if (code) {
     const saveCode = await Store.hget(`nodemail:${username}`, 'code')
@@ -43,9 +36,7 @@ router.post('/signup', async (ctx) => {
       msg: '请填写验证码'
     }
   }
-  let user = await User.find({
-    username
-  })
+  let user = await User.find({username})
   if (user.length) {
     ctx.body = {
       code: -1,
@@ -53,16 +44,9 @@ router.post('/signup', async (ctx) => {
     }
     return
   }
-  let nuser = await User.create({
-    username,
-    password,
-    email
-  })
+  let nuser = await User.create({username, password, email})
   if (nuser) {
-    let res = await axios.post('/users/signin', {
-      username,
-      password
-    })
+    let res = await axios.post('/users/signin', {username, password})
     if (res.data && res.data.code === 0) {
       ctx.body = {
         code: 0,
@@ -82,3 +66,100 @@ router.post('/signup', async (ctx) => {
     }
   }
 })
+
+router.post('/signin', async (ctx, next) => {
+  return Passport.authenticate('local', function(err, user, info, status) {
+    if (err) {
+      ctx.body = {
+        code: -1,
+        msg: err
+      }
+    } else {
+      if (user) {
+        ctx.body = {
+          code: 0,
+          msg: '登录成功',
+          user
+        }
+        return ctx.login(user)
+      } else {
+        ctx.body = {
+          code: 1,
+          msg: info
+        }
+      }
+    }
+  })(ctx, next)
+})
+
+router.post('/verify', async (ctx, next) => {
+  let username = ctx.request.body.username
+  const saveExpire = await Store.hget(`nodemail:${username}`, 'expire')
+  if (saveExpire && new Date().getTime() - saveExpire < 0) {
+    ctx.body = {
+      code: -1,
+      msg: '验证请求过于频繁，1分钟内1次'
+    }
+    return false
+  }
+  let transporter = nodeMailer.createTransport({
+    service: 'qq',
+    auth: {
+      user: Email.smtp.user,
+      pass: Email.smtp.pass
+    }
+  })
+  let ko = {
+    code: Email.smtp.code(),
+    expire: Email.smtp.expire(),
+    email: ctx.request.body.email,
+    user: ctx.request.body.username
+  }
+  let mailOptions = {
+    from: `"认证邮件" <${Email.smtp.user}>`,
+    to: ko.email,
+    subject: '《慕课网高仿美团网全栈实战》注册码',
+    html: `您在《慕课网高仿美团网全栈实战》课程中注册，您的邀请码是${ko.code}`
+  }
+  await transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error)
+    } else {
+      Store.hmset(`nodemail:${ko.user}`, 'code', ko.code, 'expire', ko.expire, 'email', ko.email)
+    }
+  })
+  ctx.body = {
+    code: 0,
+    msg: '验证码已发送，可能会有延时，有效期1分钟'
+  }
+})
+
+router.get('/exit', async (ctx, next) => {
+  await ctx.logout()
+  if (!ctx.isAuthenticated()) {
+    ctx.body = {
+      code: 0
+    }
+  } else {
+    ctx.body = {
+      code: -1
+    }
+  }
+})
+
+router.get('/getUser', async (ctx) => {
+  if (ctx.isAuthenticated()) {
+    const {username, email} = ctx.session.passport.user
+    ctx.body={
+      user:username,
+      email
+    }
+  }else{
+    ctx.body={
+      user:'',
+      email:''
+    }
+  }
+})
+
+export default router
