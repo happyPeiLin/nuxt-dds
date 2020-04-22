3e791610cce17c2100f85f95edd3ae5120dcc41d
happyPeiLin <18398322316@163.com>
2020-04-22 23:46:45 +0800

    删除了更多内容，包括之前没发现的layout里的样式

--- server/index.js
+++ server/index.js
@@ -4,17 +4,9 @@ const {Nuxt, Builder} = require('nuxt')
 
 import mongoose from 'mongoose'
 import bodyParser from 'koa-bodyparser'
-import session from 'koa-generic-session'
-import Redis from 'koa-redis'
 import json from 'koa-json'
 import dbConfig from './dbs/config'
-import passport from './interface/utils/passport'
 import users from './interface/users'
-// import geo from './interface/geo'
-// import search from './interface/search'
-// import categroy from './interface/categroy'
-// import cart from './interface/cart'
-// import order from './interface/order'
 
 const app = new Koa()
 const host = process.env.HOST || '127.0.0.1'
@@ -22,7 +14,6 @@ const port = process.env.PORT || 3000
 
 app.keys = ['mt', 'keyskeys']
 app.proxy = true
-app.use(session({key: 'mt', prefix: 'mt:uid', store: new Redis()}))
 app.use(bodyParser({
   extendTypes:['json','form','text']
 }))
@@ -31,8 +22,6 @@ app.use(json())
 mongoose.connect(dbConfig.dbs,{
   useNewUrlParser:true
 })
-app.use(passport.initialize())
-app.use(passport.session())
 
 // Import and Set Nuxt.js options
 let config = require('../nuxt.config.js')
@@ -50,11 +39,6 @@ async function start() {
     await builder.build()
   }
   app.use(users.routes()).use(users.allowedMethods())
-  // app.use(geo.routes()).use(geo.allowedMethods())
-  // app.use(search.routes()).use(search.allowedMethods())
-  // app.use(categroy.routes()).use(categroy.allowedMethods())
-  // app.use(cart.routes()).use(cart.allowedMethods())
-  // app.use(order.routes()).use(order.allowedMethods())
   app.use(ctx => {
     ctx.status = 200 // koa defaults to 404 when it sees that status is unset
 
